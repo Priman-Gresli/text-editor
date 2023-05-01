0cc2108599cbc1f8bc65ab91a1bb8e1372a83bca
priman <priman567@gmail.com>
2023-05-01 11:21:33 +0530

    remove unnecessary file

--- src/main/java/lk/ijse/dep10/app/controller/AlertFormController.java
+++ src/main/java/lk/ijse/dep10/app/controller/AlertFormController.java
@@ -1,6 +1,50 @@
 package lk.ijse.dep10.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class AlertFormController {
 
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnDoNotSave;
+
+    @FXML
+    private Button btnSave;
+    private File file;
+    private TextArea textEditor;
+    @FXML
+    private Label lblAlert;
+
+    public void initialize(){
+        String[] array={"Text Editor"};
+        if (file!=null) {
+            array = file.getPath().split("/");
+        }
+        lblAlert.setText("Save changes of document"+array[array.length-1] +"before closing?");
+    }
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDoNotSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
 
 }
